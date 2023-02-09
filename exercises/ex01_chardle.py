7130887491895142e7c5d5547e57e7d46db94993
Jackson Wright <jparrllc@ad.unc.edu>
2023-02-09 18:00:38 -0500

    exo2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,36 +2,37 @@
 __author__ = "730479825"
 word: str = input("Enter a 5-character word:")
 if (len(word) != 5):
-    print ("Error: Word must contain 5 characters")
+    print("Error: Word must contain 5 characters")
     exit()
 letter: str = input("Enter a single character:")
 if (len(letter) != 1):
-    print ("Error: Character must be a single character")
+    print("Error: Character must be a single character")
     exit()
 print("Searching for " + letter + " in " + word)
 
 matching_characters: int = 0 
-if (letter == word [0]):
-    print (letter + " found in index 0")
+if (letter == word[0]):
+    print(letter + " found at index 0")
     matching_characters = 1 + matching_characters
-if (letter == word [1]):
-    print (letter + " found in index 1")
-    matching_characters = 1 + matching_characters 
-if (letter == word [2]):
-    print (letter +  " found at index 2")
+if (letter == word[1]):
+    print(letter + " found at index 1")
     matching_characters = 1 + matching_characters
-if (letter == word [3]):
-    print (letter + " found at index 3")
+if (letter == word[2]):
+    print(letter + " found at index 2")
     matching_characters = 1 + matching_characters
-if (letter == word [4]):
-    print (letter + " found at index 4")
+if (letter == word[3]):
+    print(letter + " found at index 3")
+    matching_characters = 1 + matching_characters
+if (letter == word[4]):
+    print(letter + " found at index 4")
     matching_characters = 1 + matching_characters
 
-if (matching_characters == 0 ):
-    print ("No instances of " + letter + " found in " + word)
+if (matching_characters == 0):
+    print("No instances of " + letter + " found in " + word)
 
 if (matching_characters == 1):
-    print(str(matching_characters) + " instance of "  + letter + " found in " + word)
+    print(str(matching_characters) + " instance of " + letter + " found in " + word)
 
-else: 
-    print(str(matching_characters) + " instances of "  + letter + " found in " + word)
+else:
+    print(str(matching_characters) + " instances of " + letter + " found in " + word)
+
--- lessons/input.py
+++ lessons/input.py
@@ -3,3 +3,4 @@
 user_name: str = input("What is your name? ")
 print("Hello, " + user_name + ", good morning!")
 print("You are the best programmer ever, " + user_name)
+
